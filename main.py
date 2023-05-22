57a651a10123972c17c6daa560e93cf30befa3d2
Artur-Nayman <69670806+Artur-Nayman@users.noreply.github.com>
2023-05-22 14:56:41 +0300

    Better transleate and more features

--- main.py
+++ main.py
@@ -4,28 +4,31 @@ from telebot import types
 bot = telebot.TeleBot('6258928093:AAHhXtkLv-gnU82zkaA43j7QrvhoshU8olk')
 
 # StartBot with buttons
-@bot.message_handler(commands=["start", "help"])
-def website(message):
+@bot.message_handler(commands=["start"])
+def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     project = types.KeyboardButton("Project")
     cv = types.KeyboardButton("CV")
-    markup.add(project, cv)
-    bot.send_message(message.chat.id, "Приветсвую вас", reply_markup=markup)
+    hub = types.KeyboardButton("GitHub")
+    linked = types.KeyboardButton("LinkedIn")
+    markup.add(project, cv, hub, linked)
+    bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}!", reply_markup=markup)
 
 # Speak and answer
 @bot.message_handler()
 def get_user_text(message):
-    if message.text == "Привет":
-        bot.send_message(message.chat.id, "Чем могу помочь?", parse_mode="html")
-    elif message.text == "id":
-        bot.send_message(message.chat.id, f"Твой ID: {message.from_user.id}", parse_mode="html")
-    elif message.text == "Спасибо":
-        bot.send_message(message.chat.id, "Всегда к вашим услугам", parse_mode="html")
+    greetings = ["Hello", "Hi", "hello", "hi", "Good morning", "good morning", "Good afternoon", "Good evening", "Greetings", "Howdy", "good afternoon", "good evening", "greetings", "howdy"]
+    tynks = ["ty", "Thank you" "Thanks", "Ty", "thank you", "thanks"]
+    if any(greeting in message.text for greeting in greetings):
+        bot.send_message(message.chat.id, "How can i help you? \n\nYou can look at the list of buttons at the bottom right to select one of the available options", parse_mode="html")
+    elif any(tynks in message.text for tynks in tynks):
+        bot.send_message(message.chat.id, "Always at your service", parse_mode="html")
     elif message.text == "Project":
         # URL send
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton("Website Project", url="albatrosry.com"))
-        bot.send_message(message.chat.id, "Here is the site that I created.\nNow there is a new administrator, who deleted half of features what i added :(.", reply_markup=markup)
+        markup.add(types.InlineKeyboardButton("My Page", url="https://albatrosry.com/?page_id=1257&lang=fi"))
+        bot.send_message(message.chat.id, "Here is the site that I created.\nNow there is a new administrator, who deleted half of features what i added :( \nBut on \"My Page\" you can see proofs about that i been andministrator of that website.", reply_markup=markup)
     elif message.text == "CV":
         #CV send
         markup = types.InlineKeyboardMarkup()
@@ -35,8 +38,18 @@ def get_user_text(message):
         document_path = 'CV.pdf'
         with open(document_path, 'rb') as document:
             bot.send_document(chat_id, document)
+    elif message.text == "GitHub":
+        # URL send
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("To GitHub", url="https://github.com/Artur-Nayman"))
+        bot.send_message(message.chat.id, "Here is my GitHub", reply_markup=markup)
+    elif message.text == "LinkedIn":
+        # URL send
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Linkedin", url="https://www.linkedin.com/in/artur-nayman-98ba12200/"))
+        bot.send_message(message.chat.id, "Here is my LinkedIn", reply_markup=markup)
     else:
-        bot.send_message(message.chat.id, "Прошу прощения, я не понял команду")
+        bot.send_message(message.chat.id, "I'm sorry, I didn't understand the command")
 
 # none stop working
 bot.polling(none_stop=True)
