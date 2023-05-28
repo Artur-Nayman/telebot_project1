b6f6ea56302d01713b5a22a20209c8d92fad1758
Artur-Nayman <69670806+Artur-Nayman@users.noreply.github.com>
2023-05-28 10:19:17 +0300

    Added Eneba web site parsing Parsing !Not working buttons functions

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import telebot
 from telebot import types
+from bs4 import BeautifulSoup as BS
+from requests import get
 
-bot = telebot.TeleBot('6258928093:AAHhXtkLv-gnU82zkaA43j7QrvhoshU8olk')
+url = "https://www.eneba.com/store/xbox-game-pass?drms[]=xbox&page=1&regions[]=emea&regions[]=europe&regions[]=finland&regions[]=global&text=game%20pass%20subscription&types[]=subscription"
+bot = telebot.TeleBot('6258928093:AAERQF1wZjvEaDTBeTlVQPGAQFC_lk1KADw')
+r = get(url)
+site = BS(r.text, 'html.parser')
 
-# StartBot with buttons
-@bot.message_handler(commands=["start"])
+not_clear_game = site.find_all('span', class_="YLosEL")
+not_clear_price = site.find_all('span', class_="L5ErLT")
+game = [c.text for c in not_clear_game]
+game_price =[c.text for c in not_clear_price]
+price = [float(price[1:]) for price in game_price] #Превращает в флоат каждый элемент списка не считая превый
+filter_price = 0
+
+n_game = []
+
+def prices():
+    try:
+        count = -1
+        while count != game:
+            count = count + 1
+            if price[count] > filter_price:
+                continue
+            n_game.append(game[count])
+            n_game.append(price[count])
+    except:
+        None
+
+
+@bot.message_handler(commands=["start"])# StartBot with buttons
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     project = types.KeyboardButton("Project")
@@ -14,8 +40,31 @@ def start(message):
     markup.add(project, cv, hub, linked)
     bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}!", reply_markup=markup)
 
+@bot.message_handler(func=lambda message: True) #Xbox message
+def handle_message(message):
+    global filter_price  # Объявление глобальной переменной
+    global n_price
+    global n_game
+
+    try:
+        filter_price = float(message.text)  # Преобразование введенного значения в число
+        prices()
+        # Формирование строки с переносами строк
+        games_str = ''
+        for i, game in enumerate(n_game):
+            games_str += str(game)
+            if (i + 1) % 2 == 0:  # Проверка, является ли индекс текущего элемента вторым элементом пары
+                games_str += '\n -'
+            else:
+                games_str += '\n $'
+        bot.reply_to(message, f'Вот что я нашёл по указанной ниже цене: \n -{games_str}')
+        return
+    except ValueError:
+        None
+
+
 # Speak and answer
-@bot.message_handler()
+@bot.message_handler() #Other
 def get_user_text(message):
     greetings = ["Hello", "Hi", "hello", "hi", "Good morning", "good morning", "Good afternoon", "Good evening", "Greetings", "Howdy", "good afternoon", "good evening", "greetings", "howdy"]
     tynks = ["ty", "Thank you" "Thanks", "Ty", "thank you", "thanks"]
@@ -30,8 +79,6 @@ def get_user_text(message):
         markup.add(types.InlineKeyboardButton("My Page", url="https://albatrosry.com/?page_id=1257&lang=fi"))
         markup.add(types.InlineKeyboardButton("Second Project", url="https://artur-nayman.github.io/Asiakasty-2022v2/"))
         bot.send_message(message.chat.id, "Here is the sites that I created.\nNow in the first is a new administrator, who deleted half of features what i added :( \nBut on \"My Page\" you can see proofs about that i been andministrator of that website.\nIn second project you can see original web site created when i was project manager", reply_markup=markup)
-
-
 
     elif message.text == "CV":
         #CV send
@@ -52,6 +99,8 @@ def get_user_text(message):
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton("Linkedin", url="https://www.linkedin.com/in/artur-nayman-98ba12200/"))
         bot.send_message(message.chat.id, "Here is my LinkedIn", reply_markup=markup)
+    elif message.text == [1,2,3,4,5,6,7,8,9,0]:
+        print("nothing")
     else:
         bot.send_message(message.chat.id, "I'm sorry, I didn't understand the command")
 
